6b70c3dee0c07fca9e6342b1fc2ff69f1162a2d6
Keya Khandelwal <you@example.com>
2023-03-28 19:56:53 +0530

    first commit

--- src/components/misc/Pricing.js
+++ src/components/misc/Pricing.js
@@ -1,48 +1,56 @@
-import React from 'react'
-import './Pricing.css'
+import React from "react";
+import "./Pricing.css";
 
 const Home = () => {
-    return (
-        <React.Fragment>
-            <section className="content-container">
-            <div className="columns">
-                <ul className="price">
-                    <li className="col-header">Basic</li>
-                    <li className="grey">$ 9.99 / year</li>
-                    <li>12 consultation sessions</li>
-                    <li>1 user</li>
-                    <li>Same day audit logs</li>
-                    <li>Communiity support</li>
-                    <li className="grey"><button className="button">Sign Up</button></li>
-                </ul>
-                </div>
+  return (
+    <React.Fragment>
+      <section className="content-container">
+        <div className="columns">
+          <ul className="price">
+            <li className="col-header">Basic</li>
+            <li className="grey">$ 9.99 / year</li>
+            <li>No HD Quality</li>
+            <li>1 user</li>
+            <li>1 screen</li>
+            {/* <li>Communiity support</li> */}
+            <li className="grey">
+              <button className="button">Sign Up</button>
+            </li>
+          </ul>
+        </div>
 
-                <div className="columns">
-                <ul className="price">
-                    <li className="col-header" style={{backgroundColor:'#37387a'}}>Pro</li>
-                    <li className="grey">$ 24.99 / year</li>
-                    <li>Unlimited Consultation</li>
-                    <li>10 - 25 Users</li>
-                    <li>7 days Audit logs</li>
-                    <li>3 days SLA support</li>
-                    <li className="grey"><button className="button">Sign Up</button></li>
-                </ul>
-                </div>
+        <div className="columns">
+          <ul className="price">
+            <li className="col-header" style={{ backgroundColor: "#37387a" }}>
+              Pro
+            </li>
+            <li className="grey">$ 24.99 / year</li>
+            <li>HD Quality</li>
+            <li>2 Users</li>
+            <li>1 Screen</li>
+            {/* <li>3 days SLA support</li> */}
+            <li className="grey">
+              <button className="button">Sign Up</button>
+            </li>
+          </ul>
+        </div>
 
-                <div className="columns">
-                <ul className="price">
-                    <li className="col-header">Premium</li>
-                    <li className="grey">$ 49.99 / year</li>
-                    <li>Unlimited Consultation</li>
-                    <li>Unlimiited Users</li>
-                    <li>Audit logs with full history</li>
-                    <li>24×7×365 premium support</li>
-                    <li className="grey"><button className="button">Sign Up</button></li>
-                </ul>
-            </div>
-            </section>
-        </React.Fragment>
-    )
-}
+        <div className="columns">
+          <ul className="price">
+            <li className="col-header">Premium</li>
+            <li className="grey">$ 49.99 / year</li>
+            <li>HD Quality</li>
+            <li>4 Users</li>
+            <li>5 Screen</li>
+            {/* <li>24×7×365 premium support</li> */}
+            <li className="grey">
+              <button className="button">Sign Up</button>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </React.Fragment>
+  );
+};
 
 export default Home;
--- src/components/navigations/MenuBar.js
+++ src/components/navigations/MenuBar.js
@@ -1,23 +1,32 @@
-import React from 'react'
+import React from "react";
+import user3 from "../../images/netflixlogo.png";
 
-import './MenuBar.css'
+import "./MenuBar.css";
 
 const MenuBar = () => {
-    return (
-        <nav className="header">
-            <div className="nav-wrapper">
-                <a className="logo" href='/'>Your Logo</a>
-                <input className="menu-btn" type="checkbox" id="menu-btn"/>
-                <label className="menu-icon" htmlFor="menu-btn"><span className="navicon"></span></label>
+  return (
+    <nav className="header">
+      <div className="nav-wrapper">
+        <img src={user3} alt="Logo" style={{ width: "90px" }} />
+        <input className="menu-btn" type="checkbox" id="menu-btn" />
+        <label className="menu-icon" htmlFor="menu-btn">
+          <span className="navicon"></span>
+        </label>
 
-                <ul className="menu">
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/AboutUs">About Us</a></li>
-                    <li><a href="/ContactUs">Contact Us</a></li>                
-                </ul>
-            </div>
-        </nav>
-    )
-}
+        <ul className="menu">
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/AboutUs">About Us</a>
+          </li>
+          <li>
+            <a href="/ContactUs">Contact Us</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
 export default MenuBar;
